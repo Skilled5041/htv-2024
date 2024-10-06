24d9511fee050f75348944af263eaad9b6bf4f17
Edmond Li <liedmond0527@gmaail.com>
2024-10-06 04:33:04 +0000

    added the starter code for the domain interpretation for icp

--- src/declarations/htv-2024-backend/htv-2024-backend.did.js
+++ src/declarations/htv-2024-backend/htv-2024-backend.did.js
@@ -1,24 +1,27 @@
 export const idlFactory = ({ IDL }) => {
   const PostSnapshot = IDL.Record({
-    id: IDL.Nat,
-    title: IDL.Text,
-    creator: IDL.Text,
-    description: IDL.Text,
-    likes: IDL.Nat,
-    funding_amount: IDL.Nat,
-    images: IDL.Vec(IDL.Vec(IDL.Nat8)),
+    'id' : IDL.Nat,
+    'title' : IDL.Text,
+    'creator' : IDL.Text,
+    'description' : IDL.Text,
+    'likes' : IDL.Nat,
+    'funding_amount' : IDL.Nat,
+    'images' : IDL.Vec(IDL.Vec(IDL.Nat8)),
   });
   return IDL.Service({
-    createPost: IDL.Func(
-      [IDL.Vec(IDL.Vec(IDL.Nat8)), IDL.Text, IDL.Text, IDL.Text],
-      [],
-      []
-    ),
-    getAllPostSnapshots: IDL.Func([], [IDL.Vec(PostSnapshot)], ["query"]),
-    getPostSnapshot: IDL.Func([IDL.Nat], [PostSnapshot], ["query"]),
-    likePost: IDL.Func([IDL.Nat], [], []),
+    'createPost' : IDL.Func(
+        [IDL.Vec(IDL.Vec(IDL.Nat8)), IDL.Text, IDL.Text, IDL.Text],
+        [],
+        [],
+      ),
+    'editPost' : IDL.Func(
+        [IDL.Nat, IDL.Opt(IDL.Text), IDL.Opt(IDL.Text)],
+        [],
+        [],
+      ),
+    'getAllPostSnapshots' : IDL.Func([], [IDL.Vec(PostSnapshot)], ['query']),
+    'getPostSnapshot' : IDL.Func([IDL.Nat], [PostSnapshot], ['query']),
+    'likePost' : IDL.Func([IDL.Nat], [], []),
   });
 };
-export const init = ({ IDL }) => {
-  return [];
-};
+export const init = ({ IDL }) => { return []; };
