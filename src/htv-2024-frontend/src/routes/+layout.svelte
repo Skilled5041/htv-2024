98749ddb2d3ba470abf0a4e3065d40807e762346
Serena X <84420380+xuserena12@users.noreply.github.com>
2024-10-05 20:49:19 -0400

    added input form and navbar

--- src/htv-2024-frontend/src/routes/+layout.svelte
+++ src/htv-2024-frontend/src/routes/+layout.svelte
@@ -2,4 +2,19 @@
 	import "../app.css";
 </script>
 
+<main>
+	<nav class="p-4 bg-slate-300">
+		<div>
+			<ul class="flex justify-end gap-7">
+				<li>
+					<a href="/home">Home</a>
+				</li>
+				<li>
+					<a href="/posts">Posts</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+</main>
+
 <slot />
---
+++ src/htv-2024-frontend/src/routes/posts/new/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<main class="h-[90vh] flex items-center justify-center">
+    <div class="flex justify-center">
+        <form class="flex flex-col w-60">
+            <label for="fname" class="block mb-2 text-center">Full Name</label>
+            <input type="text" id="fname" name="fname" class="border rounded p-2 mb-4" required />
+            <label for="fname" class="block mb-2 text-center">Title</label>
+            <input type="text" id="fname" name="fname" class="border rounded p-2 mb-4" required />
+            <label for="lname" class="block mb-2 text-center">Description</label>
+            <textarea class="border rounded p-2 mb-4" required></textarea>
+            <button class="inline-block p-2 bg-blue-500 text-white rounded">Submit</button>
+        </form>
+    </div>
+</main>
